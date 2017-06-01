4617241844683bebf4652ed2304c57dfcc33f0c6
richd <richd@DESKTOP-8DS1L60>
2017-06-01 19:28:20 -0400

    Working Alpha version.
    Reads active com ports and allows user to select which port connects to an Enttec USB DMX Pro device.
    
    Allows user to enter in OSC addresses to monitor for incoming data.
    
    Allows user to connect OSC addresses to DMX slots in outgoing DMX messages.
    
    Assumes incoming OSC messages at each address contain data comprised of a single byte.
    
    Copies incoming single-byte OSC message data to outgoing DMX messages based on which DMX slot the address of the incoming message is connected to.

--- BaM Dancer Controller/src/MVC/BaMDancerHubModel.java
+++ BaM Dancer Controller/src/MVC/BaMDancerHubModel.java
@@ -5,14 +5,12 @@
  */
 package MVC;
 
-import java.util.Queue;
-import java.util.concurrent.ArrayBlockingQueue;
 import BaMDancerHub.Dancer;
-import Comm.myDataReceiveListener;
-import com.digi.xbee.api.XBeeDevice;
-import com.digi.xbee.api.exceptions.XBeeException;
+import Comm.OSCHandler;
+import Comm.XBeeHandler;
+import com.digi.xbee.api.models.XBeeMessage;
 import java.util.ArrayList;
-import java.util.concurrent.Future;
+import java.net.*;
 
 /**
  *
@@ -20,35 +18,58 @@ import java.util.concurrent.Future;
  */
 public class BaMDancerHubModel extends java.util.Observable {
     
-    XBeeDevice myXbee;
-    Queue<Future<String>> messageQueue = new ArrayBlockingQueue<>(5);
     ArrayList<Dancer> dancers = new ArrayList<>();
+    ArrayList<InetAddress> remoteHosts = new ArrayList<>();
+    XBeeHandler xbeeHandler = new XBeeHandler();
+    OSCHandler oscHandler = new OSCHandler();
+    Thread xbeeThread = new Thread(xbeeHandler, "XBee Thread");
+    Thread oscThread = new Thread(oscHandler, "OSC Thread");
 
-    /**
-     *
-     * @throws com.digi.xbee.api.exceptions.XBeeException
-     */
-    public void startXBee() throws XBeeException {
-        myXbee = new XBeeDevice("COM3", 9600);
-        myXbee.open();
-        myXbee.addDataListener(new myDataReceiveListener());
+    public void addDancer(String name, long address) {
+        Dancer dancer = new Dancer(name);
+        dancer.setMacAddress(address);
+        dancers.add(dancer);
     }
     
-    public void addDancer(String name) {
-        dancers.add(new Dancer(name));
+    public void startXbee() {
+        xbeeThread.start();
     }
     
-//    public void startXBeeListening() {
-//        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(2);
-//        Future<String> result = executor.submit(myXbee);
-//        messageQueue.offer(executor.submit(myXbee));
-//    }
+    public void parseXbeeMessage() throws InterruptedException {
+        XBeeMessage message = xbeeHandler.dequeueMessage();
+        if(message!=null) {
+            String addressString = message.getDevice().get64BitAddress().toString();
+            byte[] data = message.getData();
+            long address = hex2Long(addressString);
+            Dancer theDancer;
+            for(Dancer dancer:dancers) {
+                if(dancer.getMacAddress() == address) {
+                    for(int i = 0; i < data.length; i++) {
+                        dancer.setLevel(i, data[i]);
+                    }
+                }
+            }
+        }
+    }
     
-//    public void parseMessage() throws InterruptedException, ExecutionException {
-//        Future<String> futureMessage = messageQueue.remove();
-//        String message = futureMessage.get();
-//        String[] chunks = message.split(":");
-//        String address = chunks[0];
-//        String data = chunks[1];
-//    }
+    public void generateOSCBundles() throws UnknownHostException {
+        InetAddress localHost = InetAddress.getByAddress(new byte[] {127,0,0,1});
+        remoteHosts.add(localHost);
+        oscHandler.setHostList(remoteHosts);
+        oscHandler.setDancerList(dancers);
+        System.out.println("Start generating OSC.");
+        oscThread.start();
+    }
+    
+    private long hex2Long(String addressString) {
+        long address = 0;
+        String digits = "0123456789ABCDEF";
+        addressString = addressString.toUpperCase();
+        for(int i = 0; i < addressString.length(); i++) {
+            char c = addressString.charAt(i);
+            long d = digits.indexOf(c);
+            address = 16 * address + d;
+        }
+        return address;
+    }
 }
